72876e1749a18605e76fc4fcfbb8be55838ed105
yeshamodi12 <148933660+yeshamodi12@users.noreply.github.com>
2023-11-22 13:21:18 +0530

    done

---
+++ SQL Assignment/Section1.sql
@@ -0,0 +1,31 @@
+use sect1;
+
+/*---What does SQL stand for and what are its primary functions?
+	SQL stands for Structured query language primary functions of SQL are :-
+    1.create
+    2. update
+    3. delete
+    4. Update 
+    basically we can say that the CRUD operation is called as primary functions in SQL.*/
+    /*--Explain the difference between a primary key and a foreign key.
+    primary key means it is a collection of unique record like id, mobile no. etc.. while foreign
+    key means that you are creating the relationnship between two tables eith the help of foreign key.and also first table primary ley 
+    is the second table of foreign key.*/
+  /*  What is a subquery in SQL and when would you use it?
+    sub querey is known as query in to query with the help of subquery we can solve multiple problems with the help of subquery.*/
+    
+/* Can you describe what a view is in SQL and how it differs from a table?
+In SQL view means you can create a new table by selecting fields from one or more tables present in 
+our data base.
+syntax for view is 
+create view your newtable name as
+select your columm names 
+from your exisiting table name
+where ondition;
+
+View is containing those data which is fullfill our conditions while table 
+is storing all data of a data base.
+/*5.What is a stored procedure in SQL and what are its advantages?
+stored procedure means you an resue the command over and over. so if we 
+have to write a query again agian for that we can save as stored procedure and call it again and agian.
+
